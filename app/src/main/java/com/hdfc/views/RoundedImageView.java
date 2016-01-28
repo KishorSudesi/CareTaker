4f0544d4bcb92132a965b2cecaed9df5be2dc2fe
Balamurugan <balamurugan@adstingo.in>
2016-01-28 11:16:08 +0530

    Rounded Image View Update

--- app/src/main/java/com/hdfc/views/RoundedImageView.java
+++ app/src/main/java/com/hdfc/views/RoundedImageView.java
@@ -10,80 +10,15 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class RoundedImageView extends ImageView {
 
-   /* public RoundedImageView(Context context) {
-        super(context);
-    }
-
-    public RoundedImageView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public RoundedImageView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
-
-    public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
-        Bitmap sbmp;
-
-        if (bmp.getWidth() != radius || bmp.getHeight() != radius) {
-            float smallest = Math.min(bmp.getWidth(), bmp.getHeight());
-            float factor = smallest / radius;
-            sbmp = Bitmap.createScaledBitmap(bmp, (int) (bmp.getWidth() / factor), (int) (bmp.getHeight() / factor), false);
-        } else {
-            sbmp = bmp;
-        }
-
-        Bitmap output = Bitmap.createBitmap(radius, radius,
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xffa19774;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, radius, radius);
-
-        paint.setAntiAlias(true);
-        paint.setFilterBitmap(true);
-        paint.setDither(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(Color.parseColor("#BAB399"));
-        canvas.drawCircle(radius / 2 + 0.7f,
-                radius / 2 + 0.7f, radius / 2 + 0.1f, paint);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(sbmp, rect, rect, paint);
-
-        return output;
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-        Drawable drawable = getDrawable();
-
-        if (drawable == null) {
-            return;
-        }
-
-        if (getWidth() == 0 || getHeight() == 0) {
-            return;
-        }
-        Bitmap b = ((BitmapDrawable) drawable).getBitmap();
-        Bitmap bitmap=null;
-
-        if(b!=null) {
-            bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
-            int w = getWidth(); //, h = getHeight();
-
-            Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
-            canvas.drawBitmap(roundBitmap, 0, 0, null);
-        }
-
-    }*/
+    private static Thread backgroundThread;
+    private static Handler backgroundHandler;
 
     public RoundedImageView(Context context) {
         super(context);
@@ -124,12 +59,37 @@ public class RoundedImageView extends ImageView {
 
         Bitmap roundBitmap = getCroppedBitmap(b);
 
+        //
+        backgroundHandler = new BackgroundHandler();
+        backgroundThread = new BackgroundThread();
+        backgroundThread.start();
+        //
+
         super.setImageDrawable(new BitmapDrawable(getResources(), roundBitmap));
     }
 
     @Override
     public void setImageBitmap(Bitmap bm) {
         super.setImageBitmap(getCroppedBitmap(bm));
+    }
+
+    public class BackgroundThread extends Thread {
+        @Override
+        public void run() {
+            try {
+
+                backgroundHandler.sendEmptyMessage(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class BackgroundHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+
+        }
     }
 
 }
