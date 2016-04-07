8d350ab42d26f8a35665dba73dae8b1acc921748
Balamurugan <balamurugan@adstingo.in>
2016-04-07 20:30:23 +0530

    Data Model Revamp Register and Login Changes NDK Files CareTaker

---
+++ app/src/main/jni/obj/local/armeabi-v7a/objs/stringGen/stringGen/stringGen-jni.o.d
@@ -0,0 +1,68 @@
+obj/local/armeabi-v7a/objs/stringGen/stringGen/stringGen-jni.o: \
+ E:/AndroidStudioProjects/CareTaker/app/src/main/jni/stringGen/stringGen-jni.c \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/string.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/malloc.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdio.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdint.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_wchar_limits.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/log.h \
+ E:/AndroidStudioProjects/CareTaker/app/src/main/jni/stringGen/stringGen-jni.h
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/string.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/jni.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/_wchar_limits.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+E:/android-ndk-r10e/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+E:/AndroidStudioProjects/CareTaker/app/src/main/jni/stringGen/stringGen-jni.h:
